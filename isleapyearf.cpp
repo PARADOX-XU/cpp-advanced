6e8929610ae3c481d7b8fdc0825f6360e9c7b3a6
PARADOX-XU <65709863+PARADOX-XU@users.noreply.github.com>
2021-11-09 01:27:43 +0800

    Update isleapyearf.cpp

--- isleapyearf.cpp
+++ isleapyearf.cpp
@@ -1,22 +1,14 @@
-#include <iostream>
-#include <string>
-#include <math.h>
-using namespace std;
-
-int main(int argc, char* argv[]){
-	int year = 0;
-
-	for(int i=0; i<4; i++){
-		int temp = (int) argv[1][i];
-		year += (temp - 48) * (pow(10,3-i)); 
-		//cout<<temp - 48<<endl;
-	}
-
-	if (!(year % 400) || year % 4 == 0 && year % 100 != 0)
-		cout<<year<<" was a leap year";
+bool isleapyear(int inyear){
+	if (inyear % 4 == 0) {		
+		if (inyear % 100 == 0) {
+			if (inyear % 400 == 0)
+				return true;
+			else
+				return false;}
+		else 
+			return true;}
 	else
-		cout<<year<<" was not a leap year";
+		return false;
 
-	return 0;
-
+      return 0;
 }
