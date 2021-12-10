00b9eb719f69ac509559890b320ee473c4727c0b
PARADOX-XU <65709863+PARADOX-XU@users.noreply.github.com>
2021-12-10 08:58:08 +0800

    Create stringsplitter.cpp

---
+++ stringsplitter.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool isStrLenOdd(string s){
+    if (s.length()%2 == 0)
+        return false;
+    else
+        return true;
+}
+int main(){
+  string str,str1, str2;
+  char ch;
+  
+  cout << "\nEnter an odd length string: ";
+  
+  getline (cin, str);
+  
+  if ( ! isStrLenOdd(str)) {
+    cout << str << " Is not odd\n";
+
+    return -1;
+}
+  else {
+    cout << "Middle character: " << str[str.length()/2] << endl;
+    cout << "First half: ";
+
+    for(int i=0; i<=str.length()/2-1; i++){
+        cout << str[i];
+    }
+    cout << endl << "Second half: ";   
+
+    for (int i=str.length()/2+1; i<str.length();i++){
+        cout << str[i];
+    }    
+}
+return 0;
+}
